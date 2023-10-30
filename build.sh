03b9aa1e92cc1c5125ebea4654df6299ccbce734
Michael Guild <mg250645@ncr.com>
2023-10-30 19:34:48 -0400

    Improve build script to include flags

--- build.sh
+++ build.sh
@@ -1,62 +1,139 @@
 #!/bin/sh
 
-# 0. Tell the script to run with a production or dev environment
 prod_flag=false
+unset -v platform
 
-while test $# -gt 0; do
+usage() {
+  echo "build --platform (ios|android) [--production]"
+  echo " "
+  echo "options:"
+  echo "-h, --help                show brief help"
+  echo "--platform PLATFORM       (required) specify the platform to build"
+  echo "-p, --production          creates a production-like build"
+}
+
+has_argument() {
+    [[ ("$1" == *=* && -n ${1#*=}) || ( ! -z "$2" && "$2" != -*)  ]];
+}
+
+# TODO: Improve the android flow
+function android_build () {
+  if [ prod_flag = true ]
+  then
+    echo "Creating Android production bundle"
+    nx bundle-android --dev=false --reset-cache
+  else
+    nx bundle-android # --dev=false --reset-cache
+  fi
+
+  cd ..
+
+  if [ -e cma-android/BankingSolution/mobile/assets/main.jsbundle ]
+  then
+    echo "Removing previous bundle"
+    rm cma-android/BankingSolution/mobile/assets/main.jsbundle
+  else
+    echo "Previous bundle didn't exists"
+  fi
+
+  cp mba-react-native/dist/apps/mobile/android/main.jsbundle cma-android/BankingSolution/mobile/assets/main.jsbundle
+
+  if [ -e mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle ]
+  then
+    echo "File copied successfully"
+  fi
+
+  cd ..
+
+  # open android studio if it is not open
+  open -a /Applications/Android\ Studio.app cma-android/BankingSolution
+}
+
+function ios_build () {
+  if [ prod_flag = true ]
+  then
+    echo "Creating iOS production bundle"
+    nx bundle-ios --dev=false --reset-cache
+  else
+    nx bundle-ios
+  fi
+
+  cd ..
+
+  if [ -e mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle ]
+  then
+    echo "Removing previous bundle"
+    rm mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle
+  else
+    echo "Previous bundle didn't exists"
+  fi
+
+  cp mba-react-native/dist/apps/mobile/ios/main.jsbundle mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle
+
+  if [ -e mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle ]
+  then
+    echo "File copied successfully"
+  fi
+
+  cd ..
+
+  echo "Killing xcode..."
+  kill $(ps aux | grep 'Xcode' | awk '{print $2}')
+
+  echo "Deleting xcode cache..."
+  rm -R ~/Library/Developer/Xcode/DerivedData/*
+  xcrun simctl shutdown all
+
+  echo "Opening xcode project again!"
+  # open ~/Development/mba-ios/MobileBanking/MobileBanking.xcodeproj
+  cd ~/Development/mba-ios/
+  xed .
+}
+
+while [ $# -gt 0 ]; do
   case "$1" in
-    --production*)
-      echo "Running a production build"
-      prod_flag=true
-      break
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    --platform*)
+      if ! has_argument $@; then
+        echo "Please provide a platform" >&2
+        echo " "
+        usage
+        exit 1
+      fi
+        
+      platform=$2
+
+      shift
+      ;;
+    -p|--production)
+        prod_flag=true
+      shift
       ;;
     *)
-      echo "Running a development build"
       break
       ;;
   esac
 done
 
+if [ -z "$platform" ]
+then
+  echo 'Missing --platform (ios|android)' >&2
+  echo " "
+  usage
+  exit 1
+fi
+
 # 1. Generate the bundle and move it to the mba-ios project
-echo "Generating bundle..."
+echo "Generating ${platform} bundle..."
 cd mba-react-native
-if [ prd_flag == 'true' ]
+
+if [ platform = "ios" ]
 then
-  nx bundle-ios --dev=false --reset-cache
+  ios_build
 else
-  nx bundle-ios
-fi
+  android_build
 
-cd ..
-
-if [ -e mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle ]
-then
-  echo "Removing previous bundle"
-  rm mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle
-else
-  echo "Previous bundle didn't exists"
-fi
-
-cp mba-react-native/dist/apps/mobile/ios/main.jsbundle mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle
-
-if [ -e mba-ios/MobileBanking/Modules/ReactNative/main.jsbundle ]
-then
-  echo "File copied successfully"
-fi
-
-cd ..
-
-# 2. Stop the project in Xcode
-echo "Killing xcode..."
-kill $(ps aux | grep 'Xcode' | awk '{print $2}')
-
-# 3. Clean the build folder and restart simulator
-echo "Deleting xcode cache..."
-rm -R ~/Library/Developer/Xcode/DerivedData/*
-xcrun simctl shutdown all
-
-# 4. Rebuild the project
-echo "Opening xcode project again!"
-# open ~/Development/mba-ios/MobileBanking/MobileBanking.xcodeproj
-cd ~/Development/mba-ios/
-xed .
+echo "Wrapping up the build script..."
